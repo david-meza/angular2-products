f10090af39e4947596a075010b7274183bb24b4e
David Meza <davidmeza@outlook.com>
2016-03-27 13:40:56 -0400

    extract heroes list into service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,11 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
-
+import {HeroService} from './hero.service';
 
 @Component({
-  selector: 'my-app',
-  template:`
+    selector: 'my-app',
+    template: `
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
@@ -17,7 +17,7 @@ import {HeroDetailComponent} from './hero-detail.component';
     </ul>
     <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
-  styles:[`
+    styles: [`
     .selected {
       background-color: #CFD8DC !important;
       color: white;
@@ -66,29 +66,20 @@ import {HeroDetailComponent} from './hero-detail.component';
       border-radius: 4px 0 0 4px;
     }
   `],
-  directives: [HeroDetailComponent]
+    directives: [HeroDetailComponent],
+    providers: [HeroService]
 })
 
-export class AppComponent {
-  title = 'Tour of Heroes';
-  selectedHero: Hero;
-  
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
-  }
-  
-  heroes = HEROES;
+export class AppComponent implements OnInit {
+    title = 'Tour of Heroes';
+    heroes: Hero[];
+    selectedHero: Hero;
+    constructor(private _heroService: HeroService) { }
+    getHeroes() {
+        this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+    ngOnInit() {
+        this.getHeroes();
+    }
+    onSelect(hero: Hero) { this.selectedHero = hero; }
 }
-
-var HEROES: Hero[] = [
-  { "id": 11, "name": "Mr. Nice" },
-  { "id": 12, "name": "Narco" },
-  { "id": 13, "name": "Bombasto" },
-  { "id": 14, "name": "Celeritas" },
-  { "id": 15, "name": "Magneta" },
-  { "id": 16, "name": "RubberMan" },
-  { "id": 17, "name": "Dynama" },
-  { "id": 18, "name": "Dr IQ" },
-  { "id": 19, "name": "Magma" },
-  { "id": 20, "name": "Tornado" }
-];
